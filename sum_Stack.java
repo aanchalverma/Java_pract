b3c4ccfad390e4739d91173fbd1cce959d96b771
aanchalverma <34161377+aanchalverma@users.noreply.github.com>
2019-01-09 10:58:01 +0530

    Create sum_Stack.java

---
+++ sum_Stack.java
@@ -0,0 +1,56 @@
+//*******************************************************************
+// Welcome to CompileJava!
+// If you experience any issues, please contact us ('More Info')  -->
+// Also, sorry that the "Paste" feature no longer works! GitHub broke
+// this (so we'll switch to a new provider): https://blog.github.com\
+// /2018-02-18-deprecation-notice-removing-anonymous-gist-creation/
+//*******************************************************************
+
+import java.io.IOException;
+
+public class AdditionStack {
+   static int num;
+   static int ans;
+   static Stack theStack;
+   public static void main(String[] args)
+   
+   throws IOException {
+      num = 50;
+      stackAddition();
+      System.out.println("Sum = " + ans);
+   }
+   public static void stackAddition() {
+      theStack = new Stack(10000); 
+      ans = 0; 
+      while (num > 0) {
+         theStack.push(num); 
+         --num; 
+      }
+      while (!theStack.isEmpty()) {
+         int newN = theStack.pop(); 
+         ans += newN; 
+      }
+   }
+}
+class Stack {
+   private int maxSize; 
+   private int[] data;
+   private int top; 
+   public Stack(int s) {
+      maxSize = s;
+      data = new int[maxSize];
+      top = -1;
+   }
+   public void push(int p) {
+      data[++top] = p;
+   }
+   public int pop() {
+      return data[top--];
+   }
+   public int peek() {
+      return data[top];
+   }
+   public boolean isEmpty() {
+      return (top == -1);
+   }
+}

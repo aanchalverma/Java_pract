2e2a2b6b90785a76334aa8e8074a1085d01aef45
aanchalverma <34161377+aanchalverma@users.noreply.github.com>
2019-01-09 11:11:42 +0530

    Create LL_1.java

---
+++ LL_1.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+public class Main
+{
+	public static void main(String args[])
+    {
+    	LinkedList<String> ll = new LinkedList<String>();
+      	ll.add("15");
+      	ll.add("20");
+      	ll.add("30");
+      	System.out.print("First el:"+ll.getFirst());
+      	System.out.print("\nLast el:"+ll.getLast());
+      
+      
+    }
+
+}
